aa0413fa3f27e608aa958fbee21512013c65d64c
pattyzamsal <pazs1991@gmail.com>
2021-04-08 02:38:22 -0500

    Finish basic configuration of the project in Xcode to obtain a blank app

---
+++ Sources/Resources/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MarvelCharacters
+//
+//  Created by Patricia Zambrano on 8/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
